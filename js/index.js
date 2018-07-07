e2ca01e6c5579cca36602b67c8b75591218acdec
Fyping <18779875304@163.com>
2018-07-07 12:01:26 +0800

    update 201807

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// 
+// Inspired by JTK-Developments
+// ----
+// https://codepen.io/jtkDvlp/pen/lswmu
+//
+
+
+/*
+$(".side").on("click", "li", function(){
+  $("#cube").removeClass().addClass($(this).attr("class"));
+}).on("mouseover", "li", function(){
+  var me   = $(this);
+  var attr = me.attr("data-url");
+
+  if(attr != undefined){
+    $("h2").text(attr).css("visibility", "visible");
+  }
+});
+
+*/
---
+++ website/js/base.js
@@ -0,0 +1,7 @@
+
+function loadnew(pagename){
+	  document.getElementById("main").innerHTML = '<object type="text/html" data="'+pagename+'" width="100%" height="100%"></object>';
+}
+function loadleftmenu(){
+	var inText=" ";
+}
