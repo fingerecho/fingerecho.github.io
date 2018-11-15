fa9025105f84876eb9f015f153acc91a3c955f61
fingerecho <m13001282105@163.com>
2018-11-16 00:15:48 +0800

    Fri Nov 16 00:15:48 2018

--- gitpushall.py
+++ gitpushall.py
@@ -1,12 +1,52 @@
+#!/usr/bin/python3
+
+PROGRAM_FILE_SUFFIX = [ '.py','.cpp','.c','.log','.go']
+WEB_FILE_SUFFIX = ['.html','.htm','.js','.css','.md']
+MEDIA_FILE_SUFFIX = ['.jpg','.png','.gif',]
+SEPLINE = "======================================================"
+
+cmd=['git', # 0
+    'add',# 1
+    'commit',#2
+    'push',#3
+    '-m',#4
+    ]
+
+from subprocess import call
+from time import ctime
+from os import path , walk
+from sys import argv
+
+###########
+## Author:fingerecho
+## Date:18/10
+## Function:update git
+######################
+
+def add_file(path_:str):
+    res_ = []
+    all_LL = [ xx[2] for xx in walk(path_)]
+    all_ = []
+    for ii in all_LL:
+        all_.extend(ii)
+    #print(all_)
+    for suf_MODE in [PROGRAM_FILE_SUFFIX , WEB_FILE_SUFFIX , MEDIA_FILE_SUFFIX]:
+        for suf_ in suf_MODE:
+            [ call([cmd[0],cmd[1],file_.strip()]) for file_ in all_ if file_.strip("\n").strip().endswith(suf_)]
+    print("adding all the files that I need \n%s\n"%SEPLINE)
+def main_process(path_:str,force=""):
+    add_file(path_)
+    res_ = call([cmd[0],cmd[2],cmd[4],ctime()])
+    print(res_)
+    print("commit finished \n%s\n"%SEPLINE)
+    res_0 = call([cmd[0],cmd[3],force])
+    print("%s\nAdding result\n%s\n"%(res_0,SEPLINE))
 
 
-cmd=['git add ./*',
-'git commit -m "update  ',
-'git push',
-]
-import subprocess as sb
-import time
-print(sb.call(cmd[0]));print("execute git adding ")
-print(sb.call(cmd[1]+str(time.ctime())+" \""));print("commit finished");
-print(sb.call(cmd[2]));print("pushed")
-print("finished all executing shell")
+
+if __name__ == "__main__":
+    print("starting push all \n%s\n"%SEPLINE)
+    if len(argv) > 1 :
+        main_process(path_=".",force=argv[1])
+    else:
+        main_process(path_=".")
