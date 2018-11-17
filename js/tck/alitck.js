21497f02c1a3ba2992b09f162eca20fbd7e6a9ab
fingerecho <m13001282105@163.com>
2018-11-17 16:59:24 +0800

    udpate all

--- js/tck/alitck.js
+++ js/tck/alitck.js
@@ -1,9 +1,19 @@
 var ttu = new Array("./js/tck/tenctk.js");
+
 var addd = function(uul){
 	var tar = document.createElement("script");
 	tar.setAttribute("src",uul);
 	document.head.append(tar);
 	//$("#alitck").after(tar);	
+};
+var  json2str = function(o) { 
+	var arr = []; 
+	var fmt = function(s) { 
+	if (typeof s == 'object' && s != null) return json2str(s); 
+	return /^(string|number)$/.test(typeof s) ? "'" + s + "'" : s; 
+	} 
+	for (var i in o) arr.push("'" + i + "':" + fmt(o[i])); 
+	return '{' + arr.join(',') + '}'; 
 };
 var fask = function(params){
 
@@ -12,10 +22,12 @@ var fask = function(params){
 		type:"POST",
 		url:"https://gitee.fyping.cn:65533/serve/tck",
 		data:"params="+params,
-		success:function(msg){
-			console.log("ok"+msg);
+		success:function(data){
+			//console.log("ok"+msg);
+			var res_ = json2str(data);
+			console.log(res_);
 		},
-		error:function(msg){
+		error:function(data){
 			console.log("error");
 		}
 	});
@@ -28,63 +40,16 @@ var tarinfo = new FFGGAA().get();
 //console.log(returnCitySN["cip"]+','+returnCitySN["cname"]);
 
 var xx_user = tarinfo;//+"$$"+returnCitySN['cip']+"$$"+returnCitySN['cname']+"$$"+"1"+"";
-console.log("lenght of xx_user:"+xx_user.length);
-function getCookie(c_name)
-{
-if (document.cookie.length>0)
-  {
-  c_start=document.cookie.indexOf(c_name + "=")
-  if (c_start!=-1)
-    { 
-    c_start=c_start + c_name.length+1 
-    c_end=document.cookie.indexOf(";",c_start)
-    if (c_end==-1) c_end=document.cookie.length
-    return unescape(document.cookie.substring(c_start,c_end))
-    } 
-  }
-return ""
-}
-
-function setCookie(c_name,value,expiredays)
-{
-var exdate=new Date()
-exdate.setDate(exdate.getDate()+expiredays)
-document.cookie=c_name+ "=" +escape(value)+
-((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
-}
-
-function checkCookie()
-{
-username=getCookie('username')
-if (username!=null && username!="")
-  {
-	teem=username.split("$$");
-	var times = teem[teem.length-1];
-	var necck = "";
-	
-	for(var i=0;i<teem.length-1;i++){
-		necck=necck+teem[i];
-			necck=necck+"$$";
-		
+if(window.localStorage){
+	var times = localStorage.getItem('visit_times');
+	if (times == null || times == ""){
+		localStorage.setItem('visit_times','1');
+		xx_user += "$$1";		
+	}else{
+		times = times*1 +1;
+		localStorage.setItem('visit_times',times+"");
+		xx_user += "$$"+times+"";
 	}
-	times = times*1 + 1;
-	necck=necck+times+"";
-	/*console.log("第 "+times + "次来");*/
-	setCookie('username',necck,365);
-	console.log("necck is :"+necck)
-	fask(necck);
 }
-else 
-  {
-  /*username=prompt('Please enter your name:',"");*/
-  username = xx_user;
-  if (username!=null && username!="")
-    {
-    setCookie('username',username,365);
-    fask(username);
-    console.log("username is   :"+username)
-    }
-  }
-}
-
-checkCookie();
+console.log("xx_user:"+xx_user);
+fask(xx_user);
