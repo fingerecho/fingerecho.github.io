7bf1025c1c4875366d1d7806a10bc582bdd88fb4
fingerecho <m13001282105@163.com>
2018-11-26 20:02:40 +0800

    update  Mon Nov 26 20:02:39 2018

--- js/tck/tenctk.js
+++ js/tck/tenctk.js
@@ -1,4 +1,4 @@
-var a = function(){
+/*var a = function(){
 	console.log("helloworld");
 }();
 function _addFavorite() {
@@ -24,4 +24,81 @@ function _addFavorite() {
   else {
       alert('您的浏览器不支持,请按 Ctrl+D 手动收藏!');
   }
-}    
+}    */
+
+
+var time_send = function(){
+  // Cancel the event as stated by the standard.
+  //e.preventDefault();
+  // Chrome requires returnValue to be set.
+  //alert("hello");
+  //e.returnValue = '';
+
+  setInterval("fask();console.log('send info');",2000,true);
+
+}();
+var init = function (argument) {
+    var time_now = new Date();  
+    window.time_start = time_now.getTime();
+    //return((time_now.getTime() - clock_start)/1000); 
+    //console.log("time_start",window.time_start);
+    var time_sec = ((new Date()).getTime() - window.time_start)/1000;
+    time_sec = ""+time_sec;
+    window.time_spe = time_sec;
+};
+var fask = function(){
+  
+  var time_sec = Math.round(window.time_spe);
+  var referrer = document.referrer;
+  var location = document.location.href;
+  var language = navigator.language;
+  var random = Math.random()*100000000000000000+"";
+  var platform = navigator.platform;
+  var screen = window.screen.availWidth+"x"+window.screen.availHeight+"";
+  //console.log("send ",time_sec);
+  window.time_spe = ((new Date()).getTime() - window.time_start)/1000;
+  $.ajax({
+    type:"POST",
+    url:"https://gitee.fyping.cn:65533/serve/pgv",
+    data:"timesec="+time_sec+"&referrer="+referrer+"&location="+location+"&language="+language+"&randoms="+random+"&platform="+platform+"&screen="+screen,
+    /*beforeSend: function (XMLHttpRequest) {
+      var wel = window.welcome_info == null ? window.welcome_info : 'isnull';
+        XMLHttpRequest.setRequestHeader("Welcome", "");
+        },*/
+    success:function(data){
+      //console.log("ok"+msg);
+      //var res_ = json2str(data);
+      console.log(data);
+      eval(data);
+    },
+    error:function(data){
+      console.log("error");
+    }/*,
+     complete: function( xhr,data ){
+          // 获取相关Http Response header
+          var wpoInfo = {
+              // 服务器端时间
+              "date" : xhr.getResponseHeader('Date'),
+              // 如果开启了gzip，会返回这个东西
+              "contentEncoding" : xhr.getResponseHeader('Content-Encoding'),
+              // keep-alive ？ close？
+              "connection" : xhr.getResponseHeader('Connection'),
+              // 响应长度
+              "contentLength" : xhr.getResponseHeader('Content-Length'),
+              // 服务器类型，apache？lighttpd？
+              "server" : xhr.getResponseHeader('Server'),
+              "vary" : xhr.getResponseHeader('Vary'),
+              "transferEncoding" : xhr.getResponseHeader('Transfer-Encoding'),
+              // text/html ? text/xml?
+              "contentType" : xhr.getResponseHeader('Content-Type'),
+              "cacheControl" : xhr.getResponseHeader('Cache-Control'),
+              // 生命周期？
+              "exprires" : xhr.getResponseHeader('Exprires'),
+              "lastModified" : xhr.getResponseHeader('Last-Modified')
+              //"welcome" : xhr.getResponseHeader('Welcome')
+          };
+          window.welcome_info = wpoInfo['welcome'];
+      }*/
+  });
+};
+
