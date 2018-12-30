9c7bc06223708aaa171b0fd02ecdf215bb7f0bb7
fingerecho <m13001282105@163.com>
2018-12-30 20:15:26 +0800

    update all html

--- subcontent/generate.py
+++ subcontent/generate.py
@@ -1,36 +1,32 @@
 import os
-from shutil import copy
-from jinja2 import Environment, PackageLoader
 
-def generate(title,mainiframe,description="",\
+from jinja2 import Environment, PackageLoader
+DEBUG = True
+TEST_DIR = "test.dir"
+counts = 0 
+from glob import glob
+
+def generate(mainiframe,title="",description="",\
 	author="fingerecho,fangself.com.cn,",iconhref="./jpg/icon/favicon.gif"):
-	print(mainiframe)
+	global counts
+	counts = counts + 1
 	envi = Environment(loader=PackageLoader('pygenerate'))
-	temp = envi.get_template('html_base.html')
+	temp = envi.get_template('html_base.min.html')
 	res = temp.render(title=title,\
 		description=description,author=author,\
 		iconhref=iconhref,mainiframe=mainiframe)
-	if mainiframe.endswith(".htm"):
-#		mainiframe = mainiframe+"l"
-		mainiframe = mainiframe+"l"
-	ff = open(os.path.join("./",mainiframe),"w",encoding="utf-8")
+	page = mainiframe + "l"
+	#path = os.path.join(TEST_DIR,page)
+	if DEBUG:
+		path = os.path.join("pygenerate",TEST_DIR,page)
+	ff = open(path,"w",encoding="utf-8")
 	ff.write(res)
 	ff.close()
-def copy_rewrite(x:str): #### fix the non space of html   using pre label
-	if not x.endswith(".py"):
-		return 
-	envi = Environment(loader=PackageLoader('pygenerate'))
-	temp = envi.get_template('python_format.htm')
-	fd = open(x,"r",encoding="utf-8") 
-	txt = fd.read()
-	res = temp.render(all_code = txt)
-	fd.close()
-	fx = open(x+".htm","w+",encoding="utf-8") 
-	fx.write(res)
-	fx.close()	
+
 if __name__=="__main__":
-    files = [x for x in os.listdir() if x.endswith(".htm") and x=="linux_software_cmd.htm" ]
-    #files = [ x for x in os.listdir() if x.endswith(".py") and x=="python3_closure_nonlocal.py"]
-    #[copy_rewrite(xx) for xx in files if xx!= None and xx=="python3_closure_nonlocal.py"]
-    files = [generate("fingerecho",x) for x in files]
+	
+	files = [ generate(x) for x in os.listdir() if x.endswith(".htm")]
+	print("finished %d pages"%(counts))
+	#files = [x for x in os.listdir() if x.endswith(".htm") or x.endswith(".py") ]
+	#files = [generate("fingerecho",x) for x in files]
     
