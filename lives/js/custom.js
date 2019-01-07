01abc0da1f147fac27e118d099c9479280c55bd9
fingerecho <m13001282105@163.com>
2019-01-07 22:24:27 +0800

    change color per 3 seconds

---
+++ lives/js/custom.js
@@ -0,0 +1,12 @@
+var fh = function(){
+		var bk = document.body;
+		if (bk.style.background-color=="#f40"){
+				bk.style.background-color="#2e2256";
+		}else{
+				bk.style.background-color="#f40";
+		
+		}
+}
+function chage_background(){
+		window.setTimeout(fh,3000,false);
+}
