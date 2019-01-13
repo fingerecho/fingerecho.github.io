0a396dee73fe8165915591256677fdccbce2045f
fingerecho <m13001282105@163.com>
2019-01-13 15:53:02 +0800

    fixed the 友盟 problem

--- js/tck/tenctk.js
+++ js/tck/tenctk.js
@@ -102,4 +102,4 @@ var init = function (argument) {
 /*  });
 };
 */
-var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1275904815'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s5.cnzz.com/z_stat.php%3Fid%3D1275904815' type='text/javascript'%3E%3C/script%3E"));
+/*var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1275904815'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s5.cnzz.com/z_stat.php%3Fid%3D1275904815' type='text/javascript'%3E%3C/script%3E"));*/
