9f9e415787d0b5cd9a81834561baba96c3c192a6
fingerecho <283285356@qq.com>
2020-07-30 12:18:42 +0800

    update index.js

--- js/tck/index.js
+++ js/tck/index.js
@@ -1,149 +1,27 @@
-}
 
-//var GLOBAL_URI =  'http://127.0.0.1:65532'
+var GLOBAL_URI =  'https://sparql.fyping.cn:65533'
 
+var globaluv_uri = GLOBAL_URI + "/uv";
 
-
-var addd = function(uul){
-	document.head.append(tar);
-	//$("#alitck").after(tar);	
-};
-	var arr = []; 
-	} 
-	return '{' + arr.join(',') + '}'; 
-};
-
+var main_add_func = function(){
+	
+	//document.head.append(tar);
+	console.log("globaluv_uri:"+globaluv_uri);
+	console.log("sign:"+tarinfo);
 	$.ajax({
-		type:"POST",
-			eval(data);
-		},
-		error:function(data){
-		}
-	});
+			type:"POST",
+			url:globaluv_uri,	
+        
+			data: {
+				tokens:tarinfo
+			},
+			success: function(){
+				console.log("send post request finished....");
+			}
+		});
 };
-	/*for(var ii in ttu){
-		addd(ttu[ii]);
-	};*/
-}
 var tarinfo = new FFGGAA().get();
 
-if(window.localStorage){
-	}
-}
+main_add_func();
 
 
-
-
-function getCookie(name) {
-var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
-return r ? r[1] : undefined;
-}
-hinastyle.bid/sparql.fyping.cn/
-	var referrer = document.referrer;
-	var location = document.location.href;
-	var language = navigator.language;
-	var random = Math.random()*100000000000000000+"";
-	var platform = navigator.platform;
-　　 var pwd = localStorage.getItem("pwd");
-	if(pwd==null){
-	　　for (i = 0; i < 32; i++) {
-	　　}
-	}
-
-	return pwd;　
-	}
-	}
-	$.ajax({
-		type:"POST",
-		url:GLOBAL_URI+"/uv",	
-        },*/
-			eval(data);
-
-		},
-		error:function(data){
-		},
-		 complete: function( xhr,data ){
-	        var wpoInfo = {
-	            // 服务器端时间
-	            // 如果开启了gzip，会返回这个东西
-	            // 响应长度
-	            // 服务器类型，apache？lighttpd？
-	            // text/html ? text/xml?
-	            // 生命周期？
-	        };
-	        window.welcome_info = wpoInfo['welcome'];
-    	}
-	});
-};
-
-
-
-	function getCookie(name) {
-	var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
-	return r ? r[1] : undefined;
-	}
-	var referrer = document.referrer;
-	var location = document.location.href;
-	var language = navigator.language;
-	var random = Math.random()*100000000000000000+"";
-	var platform = navigator.platform;
-　　 var pwd = localStorage.getItem("pwd");
-	if(pwd==null){
-	　　for (i = 0; i < 32; i++) {
-	　　}
-	}
-
-	return pwd;　
-	}
-	}
-	$.ajax({
-		type:"POST",
-		url:GLOBAL_URI+"/pv",	
-        },*/
-			eval(data);*/
-			/*// Connection opened
-			});
-
-
-			});*/
-			};
-			};
-			}
-			
-
-
-		},
-		error:function(data){
-		},
-		 complete: function( xhr,data ){
-	        var wpoInfo = {
-	            // 服务器端时间
-	            // 如果开启了gzip，会返回这个东西
-	            // 响应长度
-	            // 服务器类型，apache？lighttpd？
-	            // text/html ? text/xml?
-	            // 生命周期？
-	        };
-	        window.welcome_info = wpoInfo['welcome'];
-    	}
-	});
-};
-
-
-}
-
-
-
-
-
-    var headerMap = {};
-    arr.forEach(function (line) {
-      headerMap[header] = value;
-    });
-
-    var contentType = headerMap["content-type"];
-	var date        = headerMap['Date'];
-
-  }
-}
-*/
