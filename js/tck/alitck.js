d1054e1d5060dcaa86a4b60b184bc290c47d8837
fingerecho <283285356@qq.com>
2025-11-01 19:47:00 +0800

    提交更新

--- js/tck/alitck.js
+++ js/tck/alitck.js
@@ -1,38 +1,38 @@
-//var GLOBAL_URI = "https://gitee.fyping.cn:65533"
-var GLOBAL_URI =  'http://127.0.0.1:65532'
-var GLOBAL_WS_URI = 'ws://127.0.0.1:65532'
+// //var GLOBAL_URI = "https://gitee.fyping.cn:65533"
+// var GLOBAL_URI =  'http://127.0.0.1:65532'
+// var GLOBAL_WS_URI = 'ws://127.0.0.1:65532'
 
-//var GLOBAL_URI = "https://gitee.fyping.cn:65533"
-//var GLOBAL_WS_URI = 'ws://gitee.fyping.cn:65533'
+// //var GLOBAL_URI = "https://gitee.fyping.cn:65533"
+// //var GLOBAL_WS_URI = 'ws://gitee.fyping.cn:65533'
 
-var time_sec = Math.round(window.time_spe);
-var referrer = document.referrer;
-var location = document.location.href;
-var language = navigator.language;
-var random = Math.random()*100000000000000000+"";
-var platform_ = navigator.platform;
-var screen = window.screen.availWidth+"x"+window.screen.availHeight+"";
+// var time_sec = Math.round(window.time_spe);
+// var referrer = document.referrer;
+// var location = document.location.href;
+// var language = navigator.language;
+// var random = Math.random()*100000000000000000+"";
+// var platform_ = navigator.platform;
+// var screen = window.screen.availWidth+"x"+window.screen.availHeight+"";
 
-var xsrf = getCookie("_xsrf");
-  if(xsrf=="" || xsrf==null){
-    xsrf="2|682c80a7|3d918a2ec0dcf6e9daa20ea497c27f16|1543887208";
-    xsrf="helloworld";
-  }
+// var xsrf = getCookie("_xsrf");
+//   if(xsrf=="" || xsrf==null){
+//     xsrf="2|682c80a7|3d918a2ec0dcf6e9daa20ea497c27f16|1543887208";
+//     xsrf="helloworld";
+//   }
 
-var tarinfo = new FFGGAA().get();
+// var tarinfo = new FFGGAA().get();
 
-$.ajax({
-type:"POST",
-url:GLOBAL_URI+"/pv", 
-data:"platform="+platform_+"&screen="+screen+"&_xsrf="+xsrf,
-success:function(data){
-  console.log(data);
-  eval(data);
-},
-error:function(data){
-  console.log("error");
-},
-complete: function( xhr,data ){
+// $.ajax({
+// type:"POST",
+// url:GLOBAL_URI+"/pv", 
+// data:"platform="+platform_+"&screen="+screen+"&_xsrf="+xsrf,
+// success:function(data){
+//   console.log(data);
+//   eval(data);
+// },
+// error:function(data){
+//   console.log("error");
+// },
+// complete: function( xhr,data ){
       
-  }
-});
+//   }
+// });
--- js/tck/index.js
+++ js/tck/index.js
@@ -1,27 +1,27 @@
 
-var GLOBAL_URI =  'https://sparql.fyping.cn:65534'
+// var GLOBAL_URI =  'https://sparql.fyping.cn:65534'
 
-var globaluv_uri = GLOBAL_URI + "/uv";
+// var globaluv_uri = GLOBAL_URI + "/uv";
 
-var main_add_func = function(){
+// var main_add_func = function(){
 	
-	//document.head.append(tar);
-	console.log("globaluv_uri:"+globaluv_uri);
-	console.log("sign:"+tarinfo);
-	$.ajax({
-			type:"POST",
-			url:globaluv_uri,	
+// 	//document.head.append(tar);
+// 	console.log("globaluv_uri:"+globaluv_uri);
+// 	console.log("sign:"+tarinfo);
+// 	$.ajax({
+// 			type:"POST",
+// 			url:globaluv_uri,	
         
-			data: {
-				tokens:tarinfo
-			},
-			success: function(){
-				console.log("send post request finished....");
-			}
-		});
-};
-var tarinfo = new FFGGAA().get();
+// 			data: {
+// 				tokens:tarinfo
+// 			},
+// 			success: function(){
+// 				console.log("send post request finished....");
+// 			}
+// 		});
+// };
+// var tarinfo = new FFGGAA().get();
 
-main_add_func();
+// main_add_func();
 
 
