1b44a45dc2f3387232d6951d954886f2f9a314e6
fingerecho <283285356@qq.com>
2026-09-23 14:20:27 +0800

    提交更新:增加副站www.fyping.cn

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,6 +1,5 @@
-git add ./* 
+git add ./*
 git config --global user.email "283285356@qq.com"
 git config --global user.name "fingerecho"
-git commit -m "提交更新:优化简历"
+git commit -m "提交更新:增加副站www.fyping.cn"
 git push
-
